56101a0fb5f0ba2c2a5ec0dbf64258496c35560b
gracewang723 <42806383+gracewang723@users.noreply.github.com>
2018-08-30 22:39:49 +0800

    Add files via upload

---
+++ C01 Introduction.py
@@ -0,0 +1,14 @@
+'''
+hello world
+'''
+print('Hello World!')
+
+input_word = 'Hello Python!'
+
+print('Input word is : ',input_word)
+print('Type of input_word is : ',type(input_word))
+
+input_number = 100
+
+print('Input number is : ',input_number)
+print('Type of input_number is : ',type(input_number))
